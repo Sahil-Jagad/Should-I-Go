f2f0d698443a7f8e81b51b9b20c04fa79bf3b505
Sanjana Magidewar <sanjana.magidewar@gmail.com>
2021-01-31 00:03:01 -0800

    guidelinespage

--- site/src/App.js
+++ site/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from './pages/LandingPage/LandingPage'
 import SuccessPage from './pages/SuccessPage/SuccessPage'
 import ErrorPage from './pages/ErrorPage/ErrorPage'
 import StartPage from './pages/StartPage/StartPage'
+import GuidelinesPage from './pages/GuidelinesPage/GuidelinesPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" exact component={LandingPage} />
           <Route path="/start" component={StartPage} />
           <Route path="/success" component={SuccessPage} />
+          <Route path="/guidelines" component={GuidelinesPage} />
           <Route component={ErrorPage}/>
         </Switch>
       </div>
---
+++ site/src/pages/GuidelinesPage/GuidelinesPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./GuidelinesPage.css"
+
+const GuidelinesPage = () => {
+  return (
+    <div>
+      {/* add bkgd image */}
+        {/* <div className = "landing-text">
+            <h1>CDC GUIDELINES</h1>
+        </div>
+
+        <div className = "pclass">
+            <p>Wear a Mask!</p>
+            <p>No Large Gatherings!</p>
+            <p>Wash Your Hands!</p>
+  </div> */}
+
+        <h1 class="first-class">CDC GUIDELINES</h1>
+        <h2 class="second-class">Wear a Mask!</h2>
+        <h2 class="second-class">No Large Gatherings!</h2>
+        <h2 class="second-class">Wash Your Hands!</h2>
+
+    </div>
+  )
+}
+
+export default GuidelinesPage
